0278f802dcb87790508727a4f7ecf1485e839a18
NguyenXuanHoan3205 <honanguyen2005@gmail.com>
2024-08-17 14:24:12 +0700

    Add files via upload

---
+++ Giaiphuongtrinhbacnhat.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 17 14:04:49 2024
+
+@author: Nguyen Xuan Hoan - 23694771
+"""
+
+a = float(input("Nhap gia tri cua a: "))
+b = float(input("Nhap gia tri cua b: "))
+if a == 0:
+    if b == 0:
+        print("Phuong trinh co vo so nghiem.")
+    else:
+        print("Phuong trinh vo nghiem.")
+else:
+    x = -b / a
+    print(f"Nghiem cua phuong trinh la: x = {x}")
+    
+    
